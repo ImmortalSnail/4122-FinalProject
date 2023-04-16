924206c4031773b6f32775ab32e6dd4ba3810806
ImmortalSnail <teddybrosevelt8@gmail.com>
2023-04-16 00:08:28 -0400

    update app.py

--- app.py
+++ app.py
@@ -12,28 +12,4 @@ def load_data():
 
 data = load_data()
 
-# Split the data into input features and target variable
-X = data[['feature1', 'feature2', ...]]
-y = data['target']
-
-# Train the model
-model = LinearRegression()
-model.fit(X, y)
-
-# Define a function to make predictions
-def predict(model, input):
-    prediction = model.predict(input)
-    return prediction
-
-# Define the Streamlit app
-st.title('My Streamlit App')
-st.write('This is a simple app that demonstrates how to use Streamlit with scikit-learn for regression modeling.')
-
-# Add some input widgets
-feature1 = st.slider('Feature 1', X['feature1'].min(), X['feature1'].max(), X['feature1'].mean())
-feature2 = st.slider('Feature 2', X['feature2'].min(), X['feature2'].max(), X['feature2'].mean())
-
-# Make a prediction and display the result
-input = np.array([[feature1, feature2]])
-prediction = predict(model, input)[0]
-st.write('The predicted target value is:', prediction)
+st.write(data)
