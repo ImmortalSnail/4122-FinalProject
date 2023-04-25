49d81cf081858d87eaf3cfd2cfd09cbc7373c791
ilyassafi <jsafi4428@gmail.com>
2023-04-25 14:42:38 -0400

    making the prediction

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import altair as alt
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
 from vega_datasets import data
 
 counties = alt.topo_feature(data.us_10m.url, 'counties')
@@ -11,11 +14,37 @@ states = alt.topo_feature(data.us_10m.url, feature='states')
 
 
 # Load the data
-countiesData =  pd.read_csv('us-counties.csv')
-
+#countiesData =  pd.read_csv('us-counties.csv')
+# Load the data
 stateData = pd.read_csv('COVID19_state.csv')
 
-countiesData = countiesData.sample(n=4999)
+# Add polynomial features to Deaths column
+poly_features = PolynomialFeatures(degree=2)  # You can choose the degree of polynomial regression
+X = stateData['Deaths'].values.reshape(-1, 1)  # Input feature
+X_poly = poly_features.fit_transform(X)  # Transformed feature with polynomial features
+stateData['Deaths_poly'] = X_poly[:, 1]  # Add transformed feature to stateData DataFrame
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_poly, stateData['Target_Variable'], test_size=0.2, random_state=42)
+
+# Fit polynomial regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on testing data
+y_pred = model.predict(X_test)
+
+# Calculate mean squared error
+mse = mean_squared_error(y_test, y_pred)
+print('Mean Squared Error:', mse)
+
+# Make prediction for a new input value
+new_input = np.array([[1000]])  # Example input value
+new_input_poly = poly_features.transform(new_input)
+prediction = model.predict(new_input_poly)
+print('Prediction for new input value:', prediction)
+
+
+#countiesData = countiesData.sample(n=4999)
 
 
 
@@ -28,17 +57,19 @@ radioBTN = st.radio(
 
 
 #Map showing covid Rate
+# Map showing covid Rate with polynomial features
 covid_Death_State = alt.Chart(states).mark_geoshape(
 ).encode(
-    color=alt.Color('Deaths:Q', scale=alt.Scale(scheme='reds')),
+    color=alt.Color('Deaths_poly:Q', scale=alt.Scale(scheme='reds')),  # Update the column to use polynomial feature
     tooltip=['State:O', 'Deaths:Q']
 ).transform_lookup(
     lookup='id',
-    from_=alt.LookupData(stateData, 'id', ['Deaths','State'])
+    from_=alt.LookupData(stateData, 'id', ['Deaths_poly','State'])  # Update the column to use polynomial feature
 ).project('albersUsa').properties(
     width=800,
     height=800
 )
+
 
 state_ICU  = alt.Chart(states).mark_geoshape(
 ).encode(
